c44b70df339efe5dc48d737ae5a87926d554f231
tasse00 <tasse_00@163.com>
2022-08-15 20:14:52 +0800

    fix: port config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const logger = new Logger('main');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: '*',
     },
   });
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  await app.listen(port, () => {
+    logger.log(`listen on ${port}`);
+  });
 }
 bootstrap();
