febea0793f020871895741e61664910c7fe6069a
tasse00 <tasse_00@163.com>
2022-09-06 09:18:07 +0800

    fix: get sale image timeout

--- src/gql/resolvers/nft-sale.resolver.ts
+++ src/gql/resolvers/nft-sale.resolver.ts
@@ -2,9 +2,12 @@ import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { NftSale } from '../models/nft-sale.model';
 // import { ChainService } from 'src/chain/chain.service';
 import { IpfsService } from 'src/ipfs/ipfs.service';
+import { Logger } from '@nestjs/common';
 // import { StoryService, StorySort } from 'src/story/story.service';
 @Resolver(() => NftSale)
 export class NftSalesResolver {
+  private readonly _logger = new Logger(NftSalesResolver.name);
+
   constructor(
     // private readonly _chainSvc: ChainService,
     // private readonly _storySvc: StoryService,
@@ -12,18 +15,24 @@ export class NftSalesResolver {
   ) {}
   @ResolveField('image', () => String)
   async getImage(@Parent() sale: NftSale): Promise<string> {
-    let uriPrefix = sale.uriPrefix;
+    try {
+      let uriPrefix = sale.uriPrefix;
 
-    // dirty data on dev network
-    uriPrefix = uriPrefix.replace('undefined', '');
-    uriPrefix = uriPrefix.replace(
-      'https://findtruman.io/fcc-story/ipfs/json/',
-      '',
-    );
-    //
+      // dirty data on dev network
+      uriPrefix = uriPrefix.replace('undefined', '');
+      uriPrefix = uriPrefix.replace(
+        'https://findtruman.io/fcc-story/ipfs/json/',
+        '',
+      );
+      //
 
-    const ipfsCid = uriPrefix.replace('ipfs://', '') + '/' + '1.json';
-    const dat = await this._ipfsSvc.loadJson(ipfsCid);
-    return dat.image;
+      const ipfsCid = uriPrefix.replace('ipfs://', '') + '/' + '1.json';
+
+      const dat = await this._ipfsSvc.loadJson(ipfsCid, { timeout: 5000 });
+      return dat.image;
+    } catch (err) {
+      this._logger.error('get nft-sale cid content failed', err);
+      return '';
+    }
   }
 }
