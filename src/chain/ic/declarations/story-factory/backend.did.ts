4919ee158b515fe9601dac8cd48cf7f80419552c
tasse00 <tasse_00@163.com>
2022-12-06 17:09:00 +0800

    feat: ic tasks

--- src/chain/ic/declarations/story-factory/backend.did.ts
+++ src/chain/ic/declarations/story-factory/backend.did.ts
@@ -18,6 +18,25 @@ export interface DefStory {
   cid: string;
   author: Principal;
 }
+export interface DefTask {
+  id: bigint;
+  cid: string;
+  nft: Principal;
+  status: bigint;
+  creator: Principal;
+  rewardNfts: Array<bigint>;
+  nextSubmitId: bigint;
+}
+export interface DefTaskSubmit {
+  id: bigint;
+  cid: string;
+  status: bigint;
+  creator: Principal;
+}
+export interface DefStoryTaskInfo {
+  nextTaskId: bigint;
+  storyId: bigint;
+}
 export interface Def_SERVICE {
   countSales: ActorMethod<[], bigint>;
   countStories: ActorMethod<[], bigint>;
@@ -30,10 +49,38 @@ export interface Def_SERVICE {
   >;
   publishStory: ActorMethod<[string], DefStory>;
   updateStory: ActorMethod<[bigint, string], [] | [DefStory]>;
+  cancelTask: ActorMethod<[bigint, bigint], undefined>;
+  claimAuthorReservedNft: ActorMethod<[bigint, bigint], [] | [Array<bigint>]>;
+  createTask: ActorMethod<
+    [bigint, string, Principal, Array<bigint>],
+    [] | [DefTask]
+  >;
+  createTaskSubmit: ActorMethod<[bigint, bigint, string], DefTaskSubmit>;
+  getTask: ActorMethod<[bigint, bigint], [] | [DefTask]>;
+  getTaskSubmit: ActorMethod<[bigint, bigint, bigint], [] | [DefTaskSubmit]>;
+  markTaskDone: ActorMethod<[bigint, bigint, bigint], undefined>;
+  updateTask: ActorMethod<[bigint, bigint, string], [] | [DefTask]>;
+  withdrawTaskSubmit: ActorMethod<[bigint, bigint, bigint], undefined>;
+  getStoryTaskInfo: ActorMethod<[bigint], [] | [DefStoryTaskInfo]>;
 }
 
 export const idlFactory = ({ IDL }) => {
-  const Sale: DefSale = IDL.Record({
+  const Task = IDL.Record({
+    id: IDL.Nat,
+    cid: IDL.Text,
+    nft: IDL.Principal,
+    status: IDL.Nat,
+    creator: IDL.Principal,
+    rewardNfts: IDL.Vec(IDL.Nat64),
+    nextSubmitId: IDL.Nat,
+  });
+  const TaskSubmit = IDL.Record({
+    id: IDL.Nat,
+    cid: IDL.Text,
+    status: IDL.Nat,
+    creator: IDL.Principal,
+  });
+  const Sale = IDL.Record({
     id: IDL.Nat,
     nft: IDL.Principal,
     token: IDL.Principal,
@@ -45,24 +92,60 @@ export const idlFactory = ({ IDL }) => {
     price: IDL.Nat,
     uriPrefix: IDL.Text,
   });
-  const Story: DefStory = IDL.Record({
+  const Story = IDL.Record({
     id: IDL.Nat,
     cid: IDL.Text,
     author: IDL.Principal,
   });
+  const StoryTaskInfo = IDL.Record({
+    nextTaskId: IDL.Nat,
+    storyId: IDL.Nat,
+  });
   return IDL.Service({
+    cancelTask: IDL.Func([IDL.Nat, IDL.Nat], [], ['oneway']),
+    claimAuthorReservedNft: IDL.Func(
+      [IDL.Nat, IDL.Nat],
+      [IDL.Opt(IDL.Vec(IDL.Nat64))],
+      [],
+    ),
     countSales: IDL.Func([], [IDL.Nat], ['query']),
     countStories: IDL.Func([], [IDL.Nat], ['query']),
+    createTask: IDL.Func(
+      [IDL.Nat, IDL.Text, IDL.Principal, IDL.Vec(IDL.Nat64)],
+      [IDL.Opt(Task)],
+      [],
+    ),
+    createTaskSubmit: IDL.Func([IDL.Nat, IDL.Nat, IDL.Text], [TaskSubmit], []),
     getSale: IDL.Func([IDL.Nat], [IDL.Opt(Sale)], ['query']),
     getStory: IDL.Func([IDL.Nat], [IDL.Opt(Story)], ['query']),
+    getTask: IDL.Func([IDL.Nat, IDL.Nat], [IDL.Opt(Task)], ['query']),
+    getTaskSubmit: IDL.Func(
+      [IDL.Nat, IDL.Nat, IDL.Nat],
+      [IDL.Opt(TaskSubmit)],
+      ['query'],
+    ),
+    markTaskDone: IDL.Func([IDL.Nat, IDL.Nat, IDL.Nat], [], ['oneway']),
     mintNft: IDL.Func([IDL.Nat], [IDL.Opt(IDL.Nat64)], []),
     publishNft: IDL.Func(
-      [IDL.Nat, IDL.Nat, IDL.Nat, IDL.Principal, IDL.Principal, IDL.Nat],
+      [
+        IDL.Nat,
+        IDL.Nat,
+        IDL.Nat,
+        IDL.Principal,
+        IDL.Principal,
+        IDL.Nat,
+        IDL.Text,
+        IDL.Text,
+        IDL.Text,
+      ],
       [IDL.Opt(Sale)],
       [],
     ),
     publishStory: IDL.Func([IDL.Text], [Story], []),
     updateStory: IDL.Func([IDL.Nat, IDL.Text], [IDL.Opt(Story)], []),
+    updateTask: IDL.Func([IDL.Nat, IDL.Nat, IDL.Text], [IDL.Opt(Task)], []),
+    withdrawTaskSubmit: IDL.Func([IDL.Nat, IDL.Nat, IDL.Nat], [], ['oneway']),
+    getStoryTaskInfo: IDL.Func([IDL.Nat], [IDL.Opt(StoryTaskInfo)], ['query']),
   }) as Def_SERVICE;
 };
 export const init = ({ IDL }) => {
--- src/chain/ic/ic.service.ts
+++ src/chain/ic/ic.service.ts
@@ -13,6 +13,9 @@ import {
   StoryNftService,
 } from './declarations/story-nft';
 import { NftType } from 'src/story/entities/nft-sale.entity';
+import { StoryChainTaskStatus } from 'src/story-chain-task/entities/story-chain-task.entity';
+import { StoryChainTaskSubmitStatus } from 'src/story-chain-task/entities/story-chain-task-submit.entity';
+import { chain2ett_SubmitStatus, chain2ett_TaskStatus } from '../utils';
 
 @Injectable()
 export class IcService implements Chain.ChainIntegration {
@@ -46,7 +49,7 @@ export class IcService implements Chain.ChainIntegration {
    * basic: use centralized task system
    * chain: use on-chain task system (ChainIntegration)
    */
-  taskModule: Chain.TaskModuleType = 'basic';
+  taskModule: Chain.TaskModuleType = 'chain';
 
   private _agent: HttpAgent;
   private _storyFactory: Def_SERVICE;
@@ -80,6 +83,7 @@ export class IcService implements Chain.ChainIntegration {
 
     if (enableSync) {
       this.startSyncLoop().catch((err) => {
+        console.log(err);
         this._logger.error(err);
       });
     }
@@ -159,7 +163,27 @@ export class IcService implements Chain.ChainIntegration {
     chainStoryId: string,
     chainTaskId: string,
   ): Promise<Chain.Task> {
-    return null;
+    const result = await this._storyFactory.getTask(
+      BigInt(chainStoryId),
+      BigInt(chainTaskId),
+    );
+    if (result.length == 0) {
+      return null;
+    }
+    const [task] = result;
+    const rewardNfts: string[] = [];
+    for (let idx = 0; idx < task.rewardNfts.length; idx++) {
+      rewardNfts.push(task.rewardNfts[idx].toString());
+    }
+
+    return {
+      id: task.id.toString(),
+      cid: task.cid,
+      creator: task.creator.toString(),
+      nft: task.nft.toString(),
+      rewardNfts: rewardNfts,
+      status: this.mapTaskStatus(task.status),
+    };
   }
 
   async getSubmit(
@@ -167,7 +191,21 @@ export class IcService implements Chain.ChainIntegration {
     chainTaskId: string,
     chainSubmitId: string,
   ): Promise<Chain.Submit> {
-    return null;
+    const result = await this._storyFactory.getTaskSubmit(
+      BigInt(chainStoryId),
+      BigInt(chainTaskId),
+      BigInt(chainSubmitId),
+    );
+    if (result.length == 0) {
+      return null;
+    }
+    const [submit] = result;
+    return {
+      id: submit.id.toString(),
+      cid: submit.cid,
+      creator: submit.creator.toString(),
+      status: this.mapTaskSubmitStatus(submit.status),
+    };
   }
 
   private async startSyncLoop() {
@@ -177,6 +215,7 @@ export class IcService implements Chain.ChainIntegration {
       try {
         await this.syncChainData();
       } catch (err) {
+        console.error(err);
         this._logger.error(err);
       }
       this._logger.debug(`next sync after ${syncInterval / 1000} seconds`);
@@ -209,12 +248,16 @@ export class IcService implements Chain.ChainIntegration {
     const toCreateSales: Parameters<StoryService['createNftSales']>[0] = [];
     const toUpdateSales: Parameters<StoryService['updateNftSales']>[0] = [];
     for (let storyId = 1; storyId < nextStoryId; storyId++) {
+      this._logger.debug(`[sync] try sync ${storyId}`);
       const existedStoryInDb = storiesInDb.find(
         (story) => story.chainStoryId === storyId.toString(),
       );
       // sync story
       const storyInfo = await this.getStory(storyId.toString());
-
+      if (!storyInfo) {
+        this._logger.warn(`no chainStoryId=${storyId} on chain`);
+        continue;
+      }
       if (!existedStoryInDb) {
         // not existed in db, will create story
         toCreateStories.push({
@@ -271,6 +314,9 @@ export class IcService implements Chain.ChainIntegration {
           }
         }
       }
+
+      // // sync tasks & submits;
+      // await this.syncStoryTasks(storyId);
     }
 
     this._logger.debug(
@@ -285,10 +331,180 @@ export class IcService implements Chain.ChainIntegration {
     await this._storySvc.createNftSales(toCreateSales);
     await this._storySvc.updateNftSales(toUpdateSales);
 
+    for (let storyId = 1; storyId < nextStoryId; storyId++) {
+      // sync tasks & submits;
+      await this.syncStoryTasks(storyId);
+    }
+
     this._logger.debug(`[sync] done`);
   }
 
   private async getNextStoryId(): Promise<number> {
     return +(await this._storyFactory.countStories()).toString() + 1;
+  }
+
+  private async getNextTaskId(storyId: number): Promise<number> {
+    const result = await this._storyFactory.getStoryTaskInfo(BigInt(storyId));
+    if (result.length < 1) return 1;
+    const [info] = result;
+    return parseInt(info.nextTaskId.toString());
+  }
+
+  private async getNextTaskSubmitId(
+    storyId: number,
+    taskId: number,
+  ): Promise<number> {
+    const result = await this._storyFactory.getTask(
+      BigInt(storyId),
+      BigInt(taskId),
+    );
+    if (result.length < 1) return 1;
+    const [task] = result;
+    return parseInt(task.nextSubmitId.toString());
+  }
+
+  private async syncStoryTasks(storyId: number): Promise<void> {
+    this._logger.debug(`[sync] sync tasks for ${storyId}`);
+    const nextTaskId = await this.getNextTaskId(storyId);
+
+    for (let idx = 1; idx < nextTaskId; idx++) {
+      const taskInDB = await this._chainTaskSvc.getTask({
+        chain: this.chain,
+        chainStoryId: storyId.toString(),
+        chainTaskId: idx.toString(),
+      });
+      let flag: 'pass' | 'add' | 'update' = 'pass';
+
+      if (taskInDB && taskInDB.status === StoryChainTaskStatus.Todo) {
+        flag = 'update';
+      } else if (!taskInDB) {
+        // add
+        flag = 'add';
+      }
+      if (flag === 'pass') {
+        continue;
+      }
+      // update
+      const task = await this.getTask(storyId.toString(), idx.toString());
+      if (!task) {
+        this._logger.warn(
+          `[sync] storyId ${storyId} taskId ${idx} on chain is invalid.`,
+        );
+        continue;
+      }
+
+      await this.syncStoryTaskSubmits(storyId, idx);
+
+      if (flag === 'add') {
+        this._logger.debug(`[sync] add task ${storyId}>${idx}`);
+        await this._chainTaskSvc.createTask({
+          chain: this.chain,
+          chainStoryId: storyId.toString(),
+          chainTaskId: idx.toString(),
+          creator: task.creator,
+          nft: task.nft,
+          cid: task.cid,
+          rewardNfts: task.rewardNfts,
+          status: chain2ett_TaskStatus(task.status),
+        });
+      } else {
+        // console.log('update', taskInDB, task);
+        if (
+          taskInDB.cid !== task.cid ||
+          taskInDB.status !== chain2ett_TaskStatus(task.status)
+        ) {
+          this._logger.debug(`[sync] update task ${storyId}>${idx}`);
+          await this._chainTaskSvc.updateTask({
+            chain: this.chain,
+            chainStoryId: storyId.toString(),
+            chainTaskId: task.id,
+            cid: task.cid,
+            status: chain2ett_TaskStatus(task.status),
+          });
+        }
+      }
+    }
+  }
+  private async syncStoryTaskSubmits(
+    storyId: number,
+    taskId: number,
+  ): Promise<void> {
+    this._logger.debug(`[sync] sync submits for ${storyId}>${taskId}`);
+    const nextId = await this.getNextTaskSubmitId(storyId, taskId);
+
+    for (let idx = 1; idx < nextId; idx++) {
+      const submitInDB = await this._chainTaskSvc.getSubmit({
+        chain: this.chain,
+        chainStoryId: storyId.toString(),
+        chainTaskId: taskId.toString(),
+        chainSubmitId: idx.toString(),
+      });
+      let flag: 'pass' | 'add' | 'update' = 'pass';
+      if (
+        submitInDB &&
+        submitInDB.status === StoryChainTaskSubmitStatus.PENDING
+      ) {
+        flag = 'update';
+      } else if (!submitInDB) {
+        // add
+        flag = 'add';
+      }
+      if (flag === 'pass') {
+        continue;
+      }
+      // update
+      const submit = await this.getSubmit(
+        storyId.toString(),
+        taskId.toString(),
+        idx.toString(),
+      );
+      if (!submit) {
+        this._logger.warn(
+          `[sync] storyId ${storyId} > taskId ${taskId} > submitId ${idx} on chain is invalid.`,
+        );
+        continue;
+      }
+
+      if (flag === 'add') {
+        this._logger.debug(`[sync] add submit ${storyId}>${taskId}>${idx}`);
+        await this._chainTaskSvc.createSubmit({
+          chain: this.chain,
+          chainStoryId: storyId.toString(),
+          chainTaskId: taskId.toString(),
+          chainSubmitId: idx.toString(),
+          creator: submit.creator,
+          cid: submit.cid,
+          status: chain2ett_SubmitStatus(submit.status),
+        });
+      } else {
+        if (submitInDB.status !== chain2ett_SubmitStatus(submit.status)) {
+          this._logger.debug(
+            `[sync] update submit ${storyId}>${taskId}>${idx}`,
+          );
+          await this._chainTaskSvc.updateSubmit({
+            chain: this.chain,
+            chainStoryId: storyId.toString(),
+            chainTaskId: taskId.toString(),
+            chainSubmitId: submit.id,
+            status: chain2ett_SubmitStatus(submit.status),
+          });
+        }
+      }
+    }
+  }
+
+  mapTaskStatus(status: bigint): 'TODO' | 'DONE' | 'CANCELLED' {
+    return ['TODO', 'DONE', 'CANCELLED'][parseInt(status.toString()) - 1] as
+      | 'TODO'
+      | 'DONE'
+      | 'CANCELLED';
+  }
+
+  mapTaskSubmitStatus(
+    status: bigint,
+  ): 'PENDING' | 'APPROVED' | 'REJECTED' | 'WITHDRAWED' {
+    return ['PENDING', 'APPROVED', 'WITHDRAWED'][
+      parseInt(status.toString()) - 1
+    ] as 'PENDING' | 'APPROVED' | 'REJECTED' | 'WITHDRAWED';
   }
 }
